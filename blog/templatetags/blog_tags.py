48bb5bc9faf11c8208f41a24879f900ace72a53f
Ильдар Уразбахтин <ildarurazbahtin@MacBook-Air-Ildar.local>
2025-03-14 14:45:43 +0300

    Most commeted posts to sidebar

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,6 @@
 from django import template
+from django.db.models.aggregates import Count
+
 from blog.models import Post
 
 register = template.Library()
@@ -12,4 +14,10 @@ def show_latest_posts(count = 5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
 
+
+@register.simple_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(
+        total_comments=Count('comments')
+    ).order_by('-total_comments')[:count]
 
